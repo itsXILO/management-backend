f42caec586add6b6a491cbd5d4e830de89b8e8ea
itsXILO <scienceunveiled1@gmail.com>
2026-03-14 21:49:49 +0530

    implement databbase

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+const PORT = 4000;
+app.use(express.json());
+app.get("/", (_req, res) => {
+    res.json({ message: "Server is running" });
+});
+app.listen(PORT, () => {
+    const url = `http://localhost:${PORT}`;
+    console.log(`Server started at ${url}`);
+});
+//# sourceMappingURL=index.js.map
